99a454e11de22b209be0fd01d01e53bb2daa1595
Luicel <luicelcs@gmail.com>
2024-06-06 22:07:54 -0500

    add csv importer (holy shit it actually works)

--- characters/abilities/csv/abilities_csv_importer.gd
+++ characters/abilities/csv/abilities_csv_importer.gd
@@ -5,13 +5,23 @@ class_name AbilitiesCSVImporter extends Resource
 	set(value):
 		_import()
 
+const ABILITIES_PATH = "res://characters/abilities/"
 const RESOURCE_SAVE_PATH = "res://characters/abilities/resources/"
 const CSV_FILES = [
-	preload("res://characters/abilities/csv/Wrath of the Witty - Abilities - Appearance Attack.csv")
+	preload("res://characters/abilities/csv/Wrath of the Witty - Abilities - Appearance Attack.csv"),
+	preload("res://characters/abilities/csv/Wrath of the Witty - Abilities - Intelligence Attack.csv"),
+	preload("res://characters/abilities/csv/Wrath of the Witty - Abilities - Physical Ability Attack.csv"),
+	preload("res://characters/abilities/csv/Wrath of the Witty - Abilities - Self-esteem Attack.csv"),
+	preload("res://characters/abilities/csv/Wrath of the Witty - Abilities - Social Life Attack.csv")
 ]
 
 func _import():
-	var dir = DirAccess.open(RESOURCE_SAVE_PATH).remove(RESOURCE_SAVE_PATH)
+	var dir = DirAccess.open(ABILITIES_PATH)
+	if dir != null:
+		dir.remove(RESOURCE_SAVE_PATH)
+		dir.make_dir_recursive(RESOURCE_SAVE_PATH)
+	else:
+		dir.make_dir_recursive(RESOURCE_SAVE_PATH)
 	
 	for csv_file in CSV_FILES:
 		for i in range(csv_file.records.size() - 1):
@@ -23,11 +33,15 @@ func _import():
 			ability_resource.target_type = _get_enum_of_target_type_string(row_data[3])
 			ability_resource.exhausts = row_data[4] == "TRUE"
 			ability_resource.mana_cost = int(row_data[5])
-			ability_resource.value = int(row_data[6])
-			ability_resource.attack_accuracy = _get_accuracy_percentage_from_string(row_data[7])
-			ability_resource.effect_accuracy = _get_accuracy_percentage_from_string(row_data[8])
+			if row_data[6] != "-":
+				ability_resource.value = int(row_data[6])
+			if row_data[7] != "-":
+				ability_resource.attack_accuracy = _get_accuracy_percentage_from_string(row_data[7])
+			if row_data[8] != "-":
+				ability_resource.effect_accuracy = _get_accuracy_percentage_from_string(row_data[8])
 			
 			var file_path = RESOURCE_SAVE_PATH + row_data[0] + ".tres"
+			print("Creating: " + file_path)
 			ResourceSaver.save(ability_resource, file_path)
 
 
